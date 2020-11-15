a316ba25f5201c8a5dd9767382d05c1f8a54fdf0
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-11-15 19:43:49 +0530

    adding variables java files

---
+++ string1.java
@@ -0,0 +1,10 @@
+class Hotel
+{
+	public static void main(String a[])
+	{
+		String area = "Rajajinagar";
+		String name = "Royal";
+		System.out.println(area);
+		System.out.println(name);		
+	}
+}
---
+++ stringpg.java
@@ -0,0 +1,10 @@
+class Pg
+{
+	static String area = "Rajajinagar";
+	static String name = "Guru pg";
+	public static void main(String a[])
+	{
+		System.out.println(area);
+		System.out.println(name);
+	} 
+}
