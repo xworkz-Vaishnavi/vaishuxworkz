ccda3164cedd073d74241daf796c6a650d646180
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-05 14:38:16 +0530

    adding constructor files

---
+++ music-proj/src/com/xworkz/Music.java
@@ -0,0 +1,14 @@
+package com.xworkz;
+
+public class Music {
+
+public String name;
+public String categories;
+public String musicDirector;
+	
+	
+    public	void relax()
+	{
+		System.out.println("relaxation");
+	}
+}
