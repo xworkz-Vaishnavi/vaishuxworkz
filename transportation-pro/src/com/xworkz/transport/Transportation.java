5bae4058315e3d4755a2ea46980bfbd5a2d7cd73
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-05 14:34:23 +0530

    adding constructor files

---
+++ transportation-pro/src/com/xworkz/transport/Transportation.java
@@ -0,0 +1,23 @@
+package com.xworkz.transport;
+
+public class Transportation {
+	
+public	String id;
+public	String name;
+public	String address[] = new String[6];	
+public	int noOfBranches;
+	
+	
+  public  void 	importAndExport()
+   {
+	 System.out.println("exporting from bengaluru to importing to Ballari, Mysuru");
+   }
+   
+  public  void  transport()
+   {
+	   System.out.println("Vacation to Manali");
+   }
+ 
+ 
+
+}
