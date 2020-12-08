f2af8f600dc537bb25cf1a2ce5e8ea6aa19661ce
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 22:59:41 +0530

    adding object files

---
+++ obj-atm/src/com/xworkz/atm/Atm.java
@@ -0,0 +1,14 @@
+package com.xworkz.atm;
+
+public class Atm {
+	
+public	String name="SBI";
+public	String address="gandhi nagar";
+public	int noOfBranches=7;
+	
+public	void moneyTransactions()
+	{
+		System.out.println("Transfer n withdraw of money");
+	}
+
+}
