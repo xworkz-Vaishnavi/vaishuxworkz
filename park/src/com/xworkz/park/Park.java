e80dfbb7944d8504e599f7cbc61dea311689f3a7
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 23:19:36 +0530

    adding getsetmethods

---
+++ park/src/com/xworkz/park/Park.java
@@ -0,0 +1,15 @@
+package com.xworkz.park;
+
+public class Park {
+
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+}
