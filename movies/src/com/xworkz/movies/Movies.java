30f64a15301215f6e8eed4e386e340e7d078ce13
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 23:21:39 +0530

    adding getsetmethods

---
+++ movies/src/com/xworkz/movies/Movies.java
@@ -0,0 +1,41 @@
+package com.xworkz.movies;
+
+public class Movies {
+
+	private String name;
+	private String lang;
+	private float ratings;
+	private String reviews;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getLang() {
+		return lang;
+	}
+	
+	public void setLang(String lang) {
+		this.lang=lang;
+	}
+	
+	public float getRatings() {
+		return ratings;
+	}
+	
+	public void setRatings(float ratings) {
+		this.ratings=ratings;
+	}
+	
+	public String getReviews() {
+		return reviews;
+	}
+	
+	public void setReviews(String reviews) {
+		this.reviews=reviews;
+	}
+}
