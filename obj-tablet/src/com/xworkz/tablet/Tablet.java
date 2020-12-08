9c011a03c8328a56b87e260d0221ec2a7518a820
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 23:12:39 +0530

    adding object files

---
+++ obj-tablet/src/com/xworkz/tablet/Tablet.java
@@ -0,0 +1,12 @@
+package com.xworkz.tablet;
+
+public class Tablet {
+
+public	String name="Crocin";
+public	int noOfTablets=10;
+	
+public	void cure()
+	{
+		System.out.println("Relief from headache");
+	}
+}
