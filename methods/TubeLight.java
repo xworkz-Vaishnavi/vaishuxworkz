a79e910d5ab3c53c32b1d853079e98e650aaed1e
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-09 00:30:13 +0530

    addingmethodfiles

---
+++ methods/TubeLight.java
@@ -0,0 +1,12 @@
+ class TubeLight
+ {
+	static void giveLight()
+	{
+		System.out.println("Gives the Light");
+	}
+	
+	static void giveLight(String philips)
+	{
+		System.out.println("Gives the Light" + philips);
+	}
+ }
