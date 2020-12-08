d3094ea58a9b99ec27172b6de20efbddab9c49ca
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 23:17:21 +0530

    adding getsetmethods

---
+++ rockets/src/com/xworkz/rockets/Rockets.java
@@ -0,0 +1,35 @@
+package com.xworkz.rockets;
+
+import java.util.Date;
+public class Rockets {
+	
+    private	String name;
+    private int id;
+    private Date date;
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date=date;
+	}
+	
+
+}
