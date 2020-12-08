231ce0803de56d52a229c444373ef7e143dca753
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 23:10:58 +0530

    adding object files

---
+++ obj-hospital/src/com/xworkz/hospital/Hospital.java
@@ -0,0 +1,14 @@
+package com.xworkz.hospital;
+
+public class Hospital {
+	
+public	String name = "Vims";
+public	String address = "mg road";
+public	int noOfBranches = 5;
+	
+public	void saveThePeople()
+	{
+		System.out.println("To cure the people");
+	}
+
+}
