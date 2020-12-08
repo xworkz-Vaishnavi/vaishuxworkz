00c6b18288c85d514713dd107912af0879dc6ce1
xworkz-vaishnavi <vaishnavih.xworkz@gmail.com>
2020-12-08 23:10:09 +0530

    adding object files

---
+++ obj-fan/src/com/xworkz/fan/Fan.java
@@ -0,0 +1,14 @@
+package com.xworkz.fan;
+
+public class Fan {
+	
+public	String name="crompton";
+public	String size="5 inches";
+public	double price=5000;
+	
+public	void givesAir()
+	{
+		System.out.println("Brings cool air");
+	}
+
+}
